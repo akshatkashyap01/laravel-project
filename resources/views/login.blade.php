d5aaa4508ea85e2a983d62800b790a15cc73645c
akshatkashyap01 <akshatkashyap01@gmail.com>
2024-01-14 13:21:27 +0530

    initial commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <label for="name">User name</label> -->
+    <input type="text" name="name" id="name" placeholder="userNAme">
+    <input type="password" name="password" id="password" placeholder="password">
+</body>
+</html>
